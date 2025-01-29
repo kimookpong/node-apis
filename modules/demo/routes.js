86ab6a7e8ea47c5551bf5ddecf02bd990831f530
Hakim Mudor <78400565+kimookpong@users.noreply.github.com>
2025-01-29 16:29:10 +0700

    Update routes.js

--- modules/demo/routes.js
+++ modules/demo/routes.js
@@ -3,7 +3,7 @@ const userController = require("./controllers/userController");
 const router = express.Router();
 
 router.get("/user", userController.userIndex);
-router.get("/user/:id", userController.userFind); // or put method
+router.put("/user/:id", userController.userFind); // or put method
 
 router.post("/user", userController.userIndex);
 router.patch("/user/:id", userController.userFind);
